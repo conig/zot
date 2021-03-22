7fdde9374a16427226170bbdd3f17f7056a7f2ae
conig <james.conigrave@gmail.com>
2021-03-22 14:14:25 +1100

    first

---
+++ R/read_lib.R
@@ -0,0 +1,65 @@
+#' read_library
+#'
+#' read betterbibtex json library
+#' @param path path to better biblatex debug json file
+
+# path = "C:/Users/james/Desktop/Exported Items.json"
+
+read_library <- function(path){
+  x <- jsonlite::fromJSON(path)$items
+  x$journal <- x$publicationTitle
+  x$authors <- as.character(sapply(x$creators, get_author))
+  x$authors[x$authors == "NULL"] <- ""
+  x$year <- as.numeric(sapply(stringr::str_extract_all(x$date, "\\d{4}"), function(x) x[[1]]))
+  x$note <- as.character(sapply(x$notes, get_notes))
+  x$note[x$note == "character(0)"] <- ""
+
+  out <- tibble::tibble(x[,c("citekey","year", "title", "authors", "journal", "note")])
+  out[order(-out$year, out$authors),]
+}
+
+
+get_author <- function(a){
+
+  if(length(a) == 0){
+    return("-")
+  }
+
+  if(nrow(a) == 1){
+    return(a$lastName)
+  }
+
+  if(nrow(a) == 2){
+    return(paste(a$lastName, collapse  = " & "))
+  }
+
+  #else
+
+  paste(a$lastName[1], "et al.")
+
+}
+
+get_notes <- function(n){
+  paste(n$note[[1]])
+}
+
+#' notes
+#'
+#' Create zotero notes and open them.
+#' @param path path to destination file.
+#' @param json source json
+#' @export
+
+notes <- function(path = NULL, json = NULL){
+  if(is.null(json)) json <- system.file("My Library.json", package = "zotnote")
+  lib <- read_library(json)
+  date <- Sys.Date()
+  title = "My library"
+  if(is.null(path)) path <- tempfile(fileext = ".html")
+
+  rmarkdown::render(system.file("zotero_notes.Rmd", package = "zotnote"),
+                    output_file = path,
+                    quiet = T)
+  shell.exec(path)
+
+}
